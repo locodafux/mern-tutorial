84304bc13a4ef0846cbf0d231f8cfc197c1d1649
locodafux <locodafux@gmail.com>
2023-12-15 17:14:12 +0800

    completed crud

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 // @desc Set goals
@@ -15,7 +18,11 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Set goals'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 
 })
 
@@ -23,7 +30,18 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route PUT /api/goals:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new:true
+    })
+
+    res.status(200).json(updatedGoal)
 
 })
 
@@ -31,9 +49,18 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route DELETE /api/goals:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+ await goal.deleteOne()
+    
+    res.status(200).json({id: req.params.id})
     
 })
 module.exports = {
-    getGoal,setGoal,updateGoal,deleteGoal
+    getGoals,setGoal,updateGoal,deleteGoal
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {getGoal,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
+const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
-router.route('/').get(getGoal).post(setGoal) 
+router.route('/').get(getGoals).post(setGoal) 
 router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 
